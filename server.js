025d578d5f0962cf09a8472332a67243008dc22e
wxfrv_ <xushnozamuxtorova37@gmail.com>
2026-08-19 10:31:23 +0500

    Update server.js

--- server.js
+++ server.js
@@ -56,7 +56,4 @@ app.use((err, req, res, next) => {
   res.status(500).render("errors/500");
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Assetly server ishga tushdi: http://localhost:${PORT}`);
-});
+module.exports = app;
